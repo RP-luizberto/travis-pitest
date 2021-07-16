968636814678d72806298540eb91c8900953bc76
Luiz Berto <luiz.berto@recargapay.com>
2021-07-16 12:40:25 -0300

    final change

---
+++ prepare-deploy.sh
@@ -0,0 +1,15 @@
+ORG=RP-luizberto
+REPO=RP-luizberto.github.io
+git clone "https://${GITHUB_TOKEN}@github.com/${ORG}/${REPO}.git" && echo "Cloned repo" || exit
+folders=$(echo target/pit-reports/*)
+foldername=$(basename "${folders[0]}")
+
+mv $REPO deploy_folder
+
+echo "Execution=$foldername"
+cp -r target/pit-reports/* deploy_folder
+rm -r deploy_folder/.git
+
+echo "Removed git folder"
+ls -la deploy_folder
+# TODO index html
